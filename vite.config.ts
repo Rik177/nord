73638e41e3fcd1d39bcf7f5bc2840d83bf8fa517
Rik177 <mefodi12@gmail.com>
2025-05-31 14:15:23 +0300

    Implementing Performance Optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { imagetools } from 'vite-imagetools';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    imagetools(),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png)$/i,
+      webp: {
+        quality: 85,
+      }
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    cssMinify: true,
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
   },
 });
