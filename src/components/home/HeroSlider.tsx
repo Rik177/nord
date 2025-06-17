f3cd0110034091a56b5a65f399e8a3e933c6f716
Rik177 <mefodi12@gmail.com>
2025-06-17 16:52:30 +0300

    Fix sliders and add foldable screen support

--- src/components/home/HeroSlider.tsx
+++ src/components/home/HeroSlider.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay, EffectFade, A11y } from 'swiper/modules';
 
